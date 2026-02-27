4e0419dfc81f1576762b1f203ea50975d4a315db
Harsh Karan <harshkaranofficial@gmail.com>
2026-02-27 19:51:59 +0530

    React-1

---
+++ 04_React/basic-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Title from "./Title.jsx"
+
+function App() {
+  return(
+    <>
+      <Title />
+      <Title />
+      <p> 2 x 2 = {2*2}</p>
+    </>
+  );
+}
+
+export default App
---
+++ 04_React/basic-react-app/src/Title.jsx
@@ -0,0 +1,11 @@
+import "./Title.css"
+
+function Title(){
+    return(
+        <div className="Title">
+            <h1>I m a Title</h1>
+            <p>I m a Description</p>
+        </div>
+    );
+}
+export default Title;
